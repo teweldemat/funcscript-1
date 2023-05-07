ef7c09993200128d990e8be9c09023828a042a26
Silvio Martignetti <silviomartignetti@gmail.com>
2023-05-07 17:46:01 +0300

    added more tests to GetLiteralMatch

--- funcscript.test/GetLiteralMatchTests.cs
+++ funcscript.test/GetLiteralMatchTests.cs
@@ -17,15 +17,6 @@ namespace GetLiteralMatchTests
         }
 
         [Test]
-        public void TestPartialMatch()
-        {
-            string exp = "Hello, world!";
-            int index = FuncScriptParser.GetLiteralMatch(exp, 0, "He");
-            Assert.AreEqual(2, index);
-        }
-
-
-        [Test]
         public void TestNoMatch()
         {
             string exp = "Hello, world!";
@@ -57,20 +48,50 @@ namespace GetLiteralMatchTests
             Assert.AreEqual(20, index);
         }
 
+        [Test]
+        public void TestEmptyString()
+        {
+            string exp = "";
+            int index = FuncScriptParser.GetLiteralMatch(exp, 0, "Hello");
+            Assert.AreEqual(0, index);
+        }
+
+        [Test]
+        public void TestStartIndexWithinSubstring()
+        {
+            string exp = "Hello, world!";
+            int index = FuncScriptParser.GetLiteralMatch(exp, 7, "world");
+            Assert.AreEqual(12, index);
+        }
+
+        [Test]
+        public void TestKeywordAtEndOfSubstring()
+        {
+            string exp = "Hello, world!";
+            int index = FuncScriptParser.GetLiteralMatch(exp, 12, "!");
+            Assert.AreEqual(13, index);
+        }
+
+        [Test]
+        public void TestNullString()
+        {
+            string exp = null;
+            Assert.Throws<ArgumentNullException>(() => FuncScriptParser.GetLiteralMatch(exp, 0, "Hello"));
+        }
 
         [Test]
         public void StressTest()
         {
             // Create a random generator
-            Random rnd = new Random();
+            Random rnd = new();
 
-            // Create a large input string with random 'a' characters
-            int prefixLength = 99_000_000;
-            int suffixLength = 99_000_000;
-            string exp = new string('a', prefixLength) + "Hello, world!" + new string('a', suffixLength);
+            // Create a large input string with random characters
+            int prefixLength = 99_999_999;
+            int suffixLength = 99_999_999;
+            string exp = RandomString(prefixLength, rnd) + "Hello, world!" + RandomString(suffixLength, rnd);
 
             // Create an array of keywords, including the target keyword
-            int keywordCount = 9_000_000;
+            int keywordCount = 9_999_999;
             string[] keywords = Enumerable.Range(1, keywordCount).Select(x => "kw" + x).ToArray();
 
             // Randomly place the target keyword in the array
@@ -85,8 +106,18 @@ namespace GetLiteralMatchTests
             // Check if the result is correct
             Assert.AreEqual(prefixLength + 13, index);
 
-            // Check if the execution time is reasonable (e.g., less than 5 seconds)
-            Assert.Less(timer.ElapsedMilliseconds, 5000);
+            // Check if the execution time is reasonable (e.g., less than 10 seconds)
+            Assert.Less(timer.ElapsedMilliseconds, 10000);
         }
+
+        // Generate a random string of the specified length using the provided random generator
+        private string RandomString(int length, Random rnd)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+
     }
 }
+
