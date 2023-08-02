b0b13a7ebf1453d3ffb177f3c3e0a9a357761aa7
Silvio Martignetti <silviomartignetti@gmail.com>
2023-08-02 14:37:40 +0300

    FuncScript meet SQL

---
+++ funcscript.sql/SqlFunction.cs
@@ -0,0 +1,56 @@
+﻿using funcscript.core;
+using funcscript.model;
+using System.Data.SqlClient;
+
+namespace funcscript.sql
+{
+    public class SqlFunction : IFsFunction
+    {
+        public int MaxParsCount => 2;
+
+        public CallType CallType => CallType.Prefix;
+
+        public string Symbol => "sql";
+
+        public int Precidence => 0;
+
+        public object Evaluate(IFsDataProvider parent, IParameterList pars)
+        {
+            if (pars[0] is not string connectionStr)
+                throw new InvalidOperationException($"{this.Symbol} - {ParName(0)} is required");
+
+            if (pars[1] is not string query)
+                throw new InvalidOperationException($"{this.Symbol} - {ParName(1)} is required");
+
+            using var conn = new SqlConnection(connectionStr);
+            conn.Open();
+            using var cmd = new SqlCommand(query, conn);
+            cmd.CommandTimeout = 0;
+            using var reader = cmd.ExecuteReader();
+            var results = new List<SimpleKeyValueCollection>();
+            while (reader.Read())
+            {
+                var row = new List<KeyValuePair<string, object?>>();
+                for (var i = 0; i < reader.FieldCount; i++)
+                {
+                    var value = FuncScriptSql.NormalizeDataType(reader.GetValue(i));
+                    row.Add(new KeyValuePair<string, object?>(reader.GetName(i), value));
+                }
+                results.Add(new SimpleKeyValueCollection(row.ToArray()));
+            }
+
+            var normalizedResults = FuncScript.NormalizeDataType(results);
+            return normalizedResults ?? "null";
+        }
+
+        public string? ParName(int index)
+        {
+            return index switch
+            {
+                0 => "ConnectionString",
+                1 => "Query",
+                _ => null,
+            };
+        }
+    }
+}
---
+++ funcscript/funcs/text/IsBlankFunction.cs
@@ -0,0 +1,41 @@
+﻿using funcscript.core;
+
+namespace funcscript.funcs.math
+{
+    public class IsBlankFunction : IFsFunction
+    {
+        public const string SYMBOL = "isBlank";
+        public int MaxParsCount => 1;
+
+        public CallType CallType => CallType.Prefix;
+
+        public string Symbol => SYMBOL;
+
+        public int Precidence => 100;
+
+        public object Evaluate(IFsDataProvider parent, IParameterList pars)
+        {
+            if (pars.Count < 1)
+                throw new error.TypeMismatchError($"{this.Symbol}: argument expected");
+
+            if (pars[0] == null)
+                return true;
+
+            if (pars[0] is not string str)
+                throw new error.TypeMismatchError($"{this.Symbol}: string expected");
+
+            return string.IsNullOrEmpty(str.Trim());
+        }
+
+
+
+        public string ParName(int index)
+        {
+            if (index == 0)
+            {
+                return "String";
+            }
+            return "";
+        }
+    }
+}
