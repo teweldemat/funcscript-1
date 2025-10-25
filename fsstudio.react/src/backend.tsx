c4dd3fd0115a06ebd20de049713f4406a69a27f9
tewelde <teweldemat@gmail.com>
2025-10-25 11:12:38 +0300

    clean

--- fsstudio.react/src/backend.tsx
+++
@@ -1,2 +0,0 @@
-export const SERVER_URL=process.env.REACT_APP_API_URL!
-export const SERVER_WS_URL=process.env.REACT_APP_WS_URL!
--- fsstudio.server.fileSystem/start.sh
+++
@@ -1,2 +0,0 @@
-export ASPNETCORE_ENVIRONMENT=Desktop
-dotnet fsstudio.server.fileSystem.dll --urls http://localhost:5091
