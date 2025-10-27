e94aa2201b4c7b786bf8d984176ef55923e9b3e2
tewelde <teweldemat@gmail.com>
2025-10-27 08:39:18 +0300

    reset

--- funcscript/core/IFsDataProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace funcscript.core
-{
-    public interface IFsDataProvider        
-    {
-        object Get(String name);
-        public IFsDataProvider ParentProvider { get; }
-        bool IsDefined(string key);
-    }
-}
